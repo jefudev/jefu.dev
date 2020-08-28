0037e9aef8ae375907f664e25ac49b564da8c295
Jeff Lau <jefflau@jeffs-mbp.lan>
2020-08-27 20:28:44 -0600

    smoothed out animation on cards.

--- src/components/Cards.svelte
+++ src/components/Cards.svelte
@@ -1,15 +1,11 @@
 <script>
+  import Inview from 'svelte-inview';
+  let ref;
+  let cardRatio;
+
   function flipCard() {
     this.classList.toggle('is-flipped');
   }
-
-  import Inview from 'svelte-inview';
-  import { createEventDispatcher } from 'svelte';
-
-  let ref;
-  const dispatch = createEventDispatcher();
-
-  let ratio = 0;
 </script>
 
 <style>
@@ -17,7 +13,11 @@
     width: 500px;
     height: 300px;
     perspective: 1500px;
-    transition: transform 0.6s cubic-bezier(0.65, 0, 0.35, 1);
+    transition: all 0.5s ease-in-out;
+  }
+  .card:hover {
+    transform: scale(1.11) !important;
+    opacity: 1 !important;
   }
   .card__inner {
     width: 100%;
@@ -44,40 +44,34 @@
   }
 </style>
 
-<!-- on:change={({ detail }) => dispatch('entry', {
-  verticalDirection: detail.scrollDirection.vertical,
-  ratio: detail.entry.intersectionRatio
-})} -->
-<!-- on:change={event => {
-  const { inView, entry, scrollDirection, observe, unobserve } = event.detail;
-}} -->
-<!-- -->
 <Inview
   let:inView
   let:scrollDirection
   let:entry
   wrapper={ref}
-  on:change={event => (ratio = event.detail.entry.intersectionRatio)}
+  rootMargin="-20% 0px"
+  on:change={event => (cardRatio = event.detail.entry.intersectionRatio)}
   threshold={Array.apply(null, { length: 100 }).map((n, i) => i / 100)}>
   <div
-    class="my-12 cursor-pointer card"
+    class="my-5 cursor-pointer card"
     on:click={flipCard}
     bind:this={ref}
-    style="transform: scale(calc(.75 + ({ratio}/2));">
-    <div class="card__inner">
+    style="transform: scale(min(max(calc(1.1 * {cardRatio}), 1),1.1)); opacity:
+    max(calc({cardRatio} * 1.1),.7);">
+    <div class="shadow-lg opacity-100 card__inner">
       <div
-        class="flex flex-col items-center p-4 my-4 bg-white border border-black rounded-lg card__front hover:shadow-md">
+        class="flex flex-col items-center p-4 my-4 bg-white border border-black rounded-lg card__front hover:shadow-2xl">
         <img
-          src="http://placekitten.com/150/150"
+          src="https://unsplash.it/100/100?random&gravity=center"
           class="w-16 h-16 mt-3 rounded-full"
           alt="Company Logo" />
-        <p class="p-3 text-3xl text-center">
-          {ratio} I've built and led front-end development of 6 eCommerice sites
-          from the ground up
+        <p class="p-3 text-xl text-center">
+          {cardRatio}I've built and led front-end development of 6 eCommerice
+          sites from the ground up
         </p>
       </div>
       <div
-        class="flex flex-col items-center p-4 my-4 bg-white border border-black rounded-lg card__back hover:shadow-md">
+        class="flex flex-col items-center p-4 my-4 bg-white border border-black rounded-lg card__back hover:shadow-2xl">
         <p class="p-2 text-base">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
           nemo sequi obcaecati totam repellendus? At explicabo consequatur
